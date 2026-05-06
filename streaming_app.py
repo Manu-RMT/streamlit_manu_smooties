73abbf0f3cb44e7edd84f35f2063da4e16c04f40
Manuel RAMANITRA <68026496+Manu-RMT@users.noreply.github.com>
2026-05-06 11:47:55 +0200

    Update streaming_app.py

--- streaming_app.py
+++ streaming_app.py
@@ -36,10 +36,9 @@ if ingredients_list and name_order:
 
         # Correspondance nom au pluriel ou singulier
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-      
+             
         st.subheader(fruit_chosen + ' : Nutrition Information') 
-        smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/" + fruit_chosen )  
+        smoothiefroot_response = requests.get(f"http://my.smoothiefroot.com/api/fruit/{search_on}" )  
         res_api = smoothiefroot_response.json()
         df_apismoothie = st.dataframe(data=res_api, use_container_width=True)
 
