54daf6b1c5e398b3386f19283d31d70e8b7846cc
Manuel RAMANITRA <68026496+Manu-RMT@users.noreply.github.com>
2026-05-06 09:44:34 +0200

    Update streaming_app.py

--- streaming_app.py
+++ streaming_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import *
-
+import requests  
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Custom Your Smoothie :cup_with_straw: ")
@@ -47,5 +47,7 @@ if ingredients_list and name_order:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is orderd ! ', icon=':material/check:')
 
-        
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
     
