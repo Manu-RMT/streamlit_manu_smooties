f377ca22c99f0ea5fbf3162b6d588261a1a17023
Manuel RAMANITRA <68026496+Manu-RMT@users.noreply.github.com>
2026-05-06 11:40:54 +0200

    Update streaming_app.py

--- streaming_app.py
+++ streaming_app.py
@@ -21,8 +21,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # change to pandas for use iloc
 pd_df = my_dataframe.to_pandas()
-st.write(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingedients : '
@@ -35,7 +33,11 @@ if ingredients_list and name_order:
     ingredients_str = ''
     for fruit_choosen in ingredients_list:
         ingredients_str += fruit_choosen + ' '
-        
+
+        # Correspondance nom au pluriel ou singulier
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_choosen + ' : Nutrition Information') 
         smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/" + fruit_choosen )  
         res_api = smoothiefroot_response.json()
